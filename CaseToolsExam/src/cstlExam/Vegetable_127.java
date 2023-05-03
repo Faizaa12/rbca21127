edd55e649d4abecf2916134f633114e8efb5ee11
Faizaa12 <119426570+Faizaa12@users.noreply.github.com>
2023-05-03 12:31:14 +0530

    Create Vegetable_127

--- CaseToolsExam/src/cstlExam/Vegetable_127.java
+++ CaseToolsExam/src/cstlExam/Vegetable_127.java
@@ -4,10 +4,16 @@ public class Vegetable_127 {
 	
 
 		String color;
+	String name;
 			Vegetable_127(String color)
 			{
 				this.color=color;
 		}
+	Vegetable_127(String name)
+			{
+				this.name=name;
+		}
+
 
 	}
 
